f468c69d8d4805b72ff0e7136c02486a17bf3a71
Nuddel69 <mats.bjonnes@gmail.com>
2024-06-16 02:44:20 +0200

    Simple game logic and TCP communication

--- sockClient.py
+++ sockClient.py
@@ -1,6 +1,31 @@
 import socket as sock
 
+# \x0a = Rock
+# \x0b = Paper
+# \x0c = Scissors
+
+# \x1a = Tie
+# \x1b = You win
+# \x1c = You lose
+
 def main():
+    score = {
+        b'\x1a' : 
+'''****************
+*  It's a tie! *
+****************''',
+
+        b'\x1b' : 
+'''****************
+*   You won!   *
+****************''',
+
+        b'\x1c' : 
+'''****************
+*  You lost!   *
+****************'''
+    }
+
     serverAddr = ('192.168.1.49', sock.htons(1337))
     socket_fd = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 
@@ -11,6 +36,28 @@ def main():
         print("Failed to connect to the server.")
         socket_fd.close()
         return
+
+    print("Let's play Rock, Paper, Scissors!\nReply to the prompt with either:\n[1] Rock\n[2] Paper\n[3] Scissors")
+    prompt = True
+    while prompt:
+        prompt = False
+        choice = input("> ")
+        match choice:
+            case '1':
+                socket_fd.send(b'\x0a')
+            case '2':
+                socket_fd.send(b'\x0b')
+            case '3':
+                socket_fd.send(b'\x0c')
+            case _:
+                print("Invalid input. Please enter a number between 1 and 3 to make your choice.")
+                prompt = True
+
+    while True:
+        recv = socket_fd.recv(1)
+        if recv:
+            print(score[recv])
+            break
 
     socket_fd.close()
 
--- sockServer.py
+++ sockServer.py
@@ -1,26 +1,62 @@
 import socket as sock
 
+# \x0a = Rock
+# \x0b = Paper
+# \x0c = Scissors
+
+# \x1a = Tie
+# \x1b = You win
+# \x1c = You lose
 
 def main():
+
+    rules = {
+        b'\x0a': b'\x0c',
+        b'\x0b': b'\x0a',
+        b'\x0c': b'\x0b'
+    }
+
     socket_fd = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-    socket_fd.bind(('192.168.1.49', sock.htons(1337)))
+    socket_fd.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
+    try:
+        socket_fd.bind(('192.168.1.49', sock.htons(1337)))
+    except:
+        print("Port in use. Closing...")
+        socket_fd.close()
+        return
     socket_fd.listen(5)
 
     print("Waiting for client...")
-    client = socket_fd.accept()
-
-    if client:
-        print(f"Socket connected!\nClient_Address: {client[1][0]}\tClient_Port: {client[1][1]}\n")
+    player1_sock, player1_address = socket_fd.accept()
+    if player1_sock:
+        print(f"Player 1 connected!\nClient_Address: {player1_address[0]}\tClient_Port: {player1_address[1]}\n")
     else:
         print(f"Falied to connect to client.")
         return
 
-    while True:
-        messageLen = int.from_bytes(client[0].recv(4), 'big')
-        if messageLen == 0 : break
-        message = client[0].recv(messageLen)
-        if not message : break
-        print(f"> {message.decode()}")
+    player2_sock, player2_address = socket_fd.accept()
+    if player2_sock:
+        print(f"Player 2 connected!\nClient_Address: {player2_address[0]}\tClient_Port: {player2_address[1]}\n")
+    else:
+        print(f"Falied to connect to client.")
+        return
+
+    p1_ans = player1_sock.recv(1)
+    p2_ans = player2_sock.recv(1)
+
+    if (p1_ans == p2_ans):
+        print("Tie!")
+        player1_sock.send(b'\x1a')
+        player2_sock.send(b'\x1a')
+
+    if rules[p1_ans] == p2_ans:
+        print("Player 1 wins!")
+        player1_sock.send(b'\x1b')
+        player2_sock.send(b'\x1c')
+    else:
+        print("Player 2 wins!")
+        player1_sock.send(b'\x1c')
+        player2_sock.send(b'\x1b')
 
     socket_fd.close()
 
